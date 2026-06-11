3f167e635c67b2a3cd0fee4221be2bbfe0993112
Bright Obiri Gyan <gyanobiri@gmail.com>
2026-06-11 10:10:42 +0200

    Dark mode fixed
    + upcoming; when darkmode is activated, and button changed to lightmode, clicking on lightmode button must change back to darkmode

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const massP = document.getElementById("massConvert")
 const volumeP = document.getElementById("volumeConvert")
 const lengthP = document.getElementById("lengthConvert")
 const toggleDark = document.getElementById("dark-mode-btn")
-const darkBackground = document.getElementById("darkBC")
+const container = document.getElementById("container")
+const firstConv = document.getElementsByClassName("conv-dimen")
+const colorMiniHeaders = document.getElementsByClassName("colorFirst")
+const paragraphColor = document.getElementsByClassName("conColor")
+const switchToLightMode = document.getElementById("dark-mode-btn")
+
+
+// const darkBackground = document.getElementById("darkBC")
 const convertedBackground = document.getElementsByClassName("conv-dimen")
-const switchToLightMode = document.getElementsByClassName("switchBtn")
+
 const switchToDarkMode = document.getElementById("light-mode-btn")
 
-// const toggleLight = document.getElementById("toggle-light")
 
 let numberEntered = document.getElementById("converted-value")
 
@@ -44,54 +50,26 @@ function convertbtn(){
 }
 
 
-// const heading = convertedBackground[0].querySelector("h2");
-// const paragraph = convertedBackground.querySelector("p");
-
 
 toggleDark.addEventListener("click", function(){
 
-
-    darkBackground.style.backgroundColor= "#1F2937"
-    convertedBackground[0].style.backgroundColor = "#273549"
-    convertedBackground[0].style.boxShadow = "none"
-    convertedBackground[0].style.border = "none"
-    convertedBackground[1].style.backgroundColor = "#273549"
-    convertedBackground[1].style.boxShadow = "none"
-    convertedBackground[1].style.border = "none"
-    convertedBackground[2].style.backgroundColor = "#273549"
-    convertedBackground[2].style.boxShadow = "none"
-    convertedBackground[2].style.border = "none"
-
-    convertedBackground[0].querySelectorAll("p").forEach(p => {
-    p.style.color = "#B295FF";
-    })
-    convertedBackground[1].querySelectorAll("p").forEach(p => {
-        p.style.color = "#B295FF";
-    })
-    convertedBackground[2].querySelectorAll("p").forEach(p => {
-        p.style.color = "#B295FF";
-    })
-
-    convertedBackground[0].querySelectorAll("h2").forEach(h2 => {
-        h2.style.color = "#B295FF";
-    })
-    convertedBackground[1].querySelectorAll("h2").forEach(h2 => {
-        h2.style.color = "#B295FF";
-    })
-    convertedBackground[2].querySelectorAll("h2").forEach(h2 => {
-        h2.style.color = "#B295FF";
-    })
-
     switchToLightMode.innerHTML = `
-        <button class="switchBtn" id="dark-mode-btn">
-        Light Mode
-        </button>
-    `;
+                                    <button class="switchBtn" id="dark-mode-btn">
+                                    Light Mode
+                                     </button>
+                                    `
+    container.classList.toggle("darkBC")
+    firstConv[0].classList.toggle("darkconversionBC")
+    firstConv[1].classList.toggle("darkconversionBC")
+    firstConv[2].classList.toggle("darkconversionBC")
+    colorMiniHeaders[0].classList.toggle("colorSecond")
+    colorMiniHeaders[1].classList.toggle("colorSecond")
+    colorMiniHeaders[2].classList.toggle("colorSecond")
+    paragraphColor[0].classList.toggle("paraColor")
+    paragraphColor[1].classList.toggle("paraColor")
+    paragraphColor[2].classList.toggle("paraColor")
+
     
 })
 
 
-
-switchToDarkMode.addEventListener("click", function(){
-
-})
